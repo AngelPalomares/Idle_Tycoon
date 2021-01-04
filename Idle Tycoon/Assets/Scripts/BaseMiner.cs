d7c3dc17c2ac20b82b9c5db7deca94d191d5e5a5
AngelPalomares <64100727+AngelPalomares@users.noreply.github.com>
2021-01-04 17:25:24 -0600

    IdleTycoon

---
+++ Idle Tycoon/Assets/Scripts/BaseMiner.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class BaseMiner : MonoBehaviour
+{
+    private float MoveSpeed = 5f;
+    private int initialCollectCapacity = 200;
+    private float goldCollectPerSecond = 50f;
+
+    public int currentGold { get; set; }
+
+    public int CollectCapacity { get; set; }
+    public float CollectPerSecond { get; set; }
+
+    public bool IsTimeToCollect { get; set; }
+
+    private void Awake()
+    {
+        IsTimeToCollect = true;
+        currentGold = 0;
+        CollectCapacity = initialCollectCapacity;
+        CollectPerSecond = goldCollectPerSecond;
+    }
+    public virtual void MoveMiner(Vector3 NewPosition)
+    {
+        transform.DOMove(NewPosition, duration: 10f / MoveSpeed).OnComplete((() => 
+        {
+            if (IsTimeToCollect)
+            {
+                CollectGold();
+            }
+            else
+            {
+                DepositGold();
+            }
+        })).Play();
+    }
+
+    protected virtual void CollectGold()
+    {
+
+    }
+
+    protected virtual IEnumerator IECollect(int CollectGold,float collectTime)
+    {
+        yield return null;
+    }
+
+    protected virtual void DepositGold()
+    {
+
+    }
+
+    public void RotateMiner(int direction)
+    {
+        if(direction == 1)
+        {
+            transform.localScale = new Vector3(x: 1, y: 1, z: 1);
+        }
+        else
+        {
+            transform.localScale = new Vector3(x: -1, y: 1, z: 1);
+        }
+    }
+
+    public void ChangeGoal()
+    {
+        IsTimeToCollect = !IsTimeToCollect;
+    }
+}
---
+++ Idle Tycoon/Assets/Scripts/ShaftMiner.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaftMiner : BaseMiner
+{
+    [SerializeField]
+    private Transform ShaftMiningLocation;
+    [SerializeField] private Transform shaftDepositLocation;
+
+    private Animator Anim;
+
+    private void Start()
+    {
+        Anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.M))
+        {
+            MoveMiner(ShaftMiningLocation.position);
+        }
+    }
+
+    public override void MoveMiner(Vector3 NewPosition)
+    {
+        base.MoveMiner(NewPosition);
+        Anim.SetTrigger("Walking");
+    }
+
+    protected override void CollectGold()
+    {
+        float collectTime = CollectCapacity / CollectPerSecond;
+
+        Anim.SetTrigger("Minning");
+        StartCoroutine(IECollect(CollectCapacity, collectTime));
+
+    }
+
+    protected override IEnumerator IECollect(int CollectGold,float collectTime)
+    {
+        yield return new WaitForSeconds(collectTime);
+        //Update Current Gold
+        currentGold = CollectGold;
+        ChangeGoal();
+        RotateMiner(direction:-1);
+        MoveMiner(shaftDepositLocation.position);
+    }
+
+    protected override void DepositGold()
+    {
+        // Add CurrentGold to the Deposit(class)
+        // Update some Values
+        currentGold = 0;
+        ChangeGoal();
+        RotateMiner(1);
+        MoveMiner(ShaftMiningLocation.position);
+    }
+}
